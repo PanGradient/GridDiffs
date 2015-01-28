7eb4fa2ec7c90693785c46a060139c687cfe0623
PanGradient <PanGradient@gmail.com>
2015-01-28 02:05:47 +0100

    Adding and correcting some comments.

--- src/CartesianLaplacian.cc
+++ src/CartesianLaplacian.cc
@@ -1,3 +1,12 @@
+/*
+ * File: CartesianLaplacian.cc
+ * Author(s): P Kuszaj
+ * Last changed: 28.01.2015
+ *
+ * A source file providing CartesianLaplacian class methods implementation
+ * (declared in CartesianLaplacian.h header file).
+ */
+
 #include "CartesianLaplacian.h"
 
 namespace GridDiff
@@ -7,6 +16,7 @@ double CartesianLaplacian::eval(const QGrid & xVals,
                                 const QGrid & yVals,
                                 const QGrid & zVals)
 {
+    /* Lf(x,y,z) = d^2f/dx^2 + d^2f/dy^2 +  d^2f/dz^2 */
     return   fEvalQ1Diff(2, xVals)
            + fEvalQ2Diff(2, yVals)
            + fEvalQ3Diff(2, zVals);
--- src/CartesianLaplacian.h
+++ src/CartesianLaplacian.h
@@ -1,10 +1,13 @@
 /*
  * File: CartesianLaplacian.h
  * Author(s): P Kuszaj
- * Last changed: 27.01.2015
+ * Last changed: 28.01.2015
  *
- * 
- * */
+ * A header file prividing with CartesianLaplacian class used for evaluating
+ * Laplace operator at a given point in cartesian coordinate system using
+ * given grid points along x, y and z axes. For further information please
+ * see basic_3D_diffop.h header file.
+ */
 
 #ifndef GRIDDIFF_CARTESIANLAPLACIAN_H
 #define GRIDDIFF_CARTESIANLAPLACIAN_H
@@ -17,11 +20,21 @@ namespace GridDiff
 /*
  * CartesianLaplacian class
  *
- * 
+ * Allows evaluation of Laplace operator at a given point in cartesian
+ * coordinate system using given grid points along x, y and z axes.
  */
 class CartesianLaplacian : public Basic_3D_DiffOp
 {
     public:
+        /*************
+         * LIFECYCLE *
+         ************/
+
+        /*
+         * Since the only new class member is evaluation method,
+         * constructors simply call the parent class constructors.
+         * For further information please see basic_3D_diffop.h header file.
+         */
         CartesianLaplacian (const QPoint   & r0Point,
                             const QGrid    & xCoords,
                             const QGrid    & yCoords,
@@ -37,6 +50,44 @@ class CartesianLaplacian : public Basic_3D_DiffOp
 
                                    Basic_3D_DiffOp (other) { }
 
+        /*************
+         * OPERATORS *
+         ************/
+
+        /*
+         * Inherited: 
+         * Basic_3D_DiffOp & operator= (const Basic_3D_DiffOp & other);
+         */
+
+        /**************
+         * OPERATIONS *
+         **************/
+
+        /*
+         * eval()
+         *
+         * Evaluation method. Takes function values at grid points at axes
+         * x, y and z. Their order has to correspond to the grid points
+         * order.
+         *
+         * -----------
+         *  Arguments
+         * -----------
+         * const QGrid & xVals
+         * const QGrid & yVals
+         * const QGrid & zVals
+         *     Function values at grid points at axes x, y and z.
+         *
+         * ---------
+         *  Returns
+         * ---------
+         * Double value equal to numerical Laplace operator at given point.
+         *
+         * ------------
+         *  Exceptions
+         * ------------
+         * None.
+         */
         double eval(const QGrid & xVals,
                     const QGrid & yVals,
                     const QGrid & zVals);
