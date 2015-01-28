64d6828b97adcef3f40372d1032b6f8169a0e1b6
PanGradient <PanGradient@gmail.com>
2015-01-28 14:30:29 +0100

    Added header file with all Laplace operator classes

---
+++ src/Laplacians.h
@@ -0,0 +1,17 @@
+/*
+ * File: CartesianLaplacian.h
+ * Author(s): P Kuszaj
+ * Last changed: 28.01.2015
+ *
+ * A header file including classes describing Laplace operator in multiple
+ * orthogonal coordinate systems in R^3.
+ */
+
+#ifndef GRIDDIFF_LAPLACIANS_H
+#define GRIDDIFF_LAPLACIANS_H
+
+#include "CartesianLaplacian.h"   /* cartesian */
+#include "CylindricalLaplacian.h" /* cylindrical */
+#include "SphericalLaplacian.h"   /* spherical */
+
+#endif /* GRIDDIFF_LAPLACIANS_H */
