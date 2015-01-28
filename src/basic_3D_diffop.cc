64e2b7d3d756cd4b12c1b92b750d19ef45af49e4
PanGradient <PanGradient@gmail.com>
2015-01-28 01:29:06 +0100

    Implementing copy constructor, correcting some bugs, adding comments

--- src/basic_3D_diffop.cc
+++ src/basic_3D_diffop.cc
@@ -13,6 +13,7 @@ Basic_3D_DiffOp::Basic_3D_DiffOp (const QPoint   &  q0Point,
                                   const QGrid    & q3Coords,
                                   const unsigned & MaxOrder)
 {
+    /* Initializing members (in case of exception accurence). */
     mQ0Point.q1 = 0.0; mQ0Point.q2 = 0.0; mQ0Point.q3 = 0.0; 
 
     mQ1Coords = QGrid();
@@ -25,6 +26,7 @@ Basic_3D_DiffOp::Basic_3D_DiffOp (const QPoint   &  q0Point,
 
     mMaxOrder = 0;
 
+    /* If one of arguments is invalid, throw exception. */
     if (q1Coords.size() <= 2){
         throw std::invalid_argument("q1 grid size < 2");
     }
@@ -45,16 +47,22 @@ Basic_3D_DiffOp::Basic_3D_DiffOp (const QPoint   &  q0Point,
         throw std::invalid_argument("q3 grid size < max deriv. order");
     }
 
-    mQ0Point.q1 = q0Point.q1; mQ0Point.q2 = q0Point.q2; mQ0Point.q3 = q0Point.q3;
+    /* Setting members to argument values. */
+    mQ0Point.q1 = q0Point.q1;
+    mQ0Point.q2 = q0Point.q2;
+    mQ0Point.q3 = q0Point.q3;
 
     mQ1Coords = q1Coords;
     mQ2Coords = q2Coords;
     mQ3Coords = q3Coords;
 
+    /* Allocating coefficient arrays. */
     pQ1Coeffs = new double [ q1Coords.size() * (MaxOrder+1) ];
     pQ2Coeffs = new double [ q2Coords.size() * (MaxOrder+1) ];
     pQ3Coeffs = new double [ q3Coords.size() * (MaxOrder+1) ];
 
+    /* Calculating coefficient values up to the derivative of order
+     * MaxOrder. */
     FornbergNumDerivsCoeffs(pQ1Coeffs,
                             q0Point.q1, &q1Coords[0],
                             q1Coords.size(), MaxOrder+1);
@@ -67,12 +75,69 @@ Basic_3D_DiffOp::Basic_3D_DiffOp (const QPoint   &  q0Point,
                             q0Point.q3, &q3Coords[0],
                             q3Coords.size(), MaxOrder+1);
 
+    /* Setting mMaxOrder. */
     mMaxOrder = MaxOrder;
+}
+
+
+Basic_3D_DiffOp::Basic_3D_DiffOp (const Basic_3D_DiffOp & other)
+{
+    /* Copy members. */
+    mQ0Point.q1 = other.mQ0Point.q1;
+    mQ0Point.q2 = other.mQ0Point.q2;
+    mQ0Point.q3 = other.mQ0Point.q3;
+
+    mQ1Coords = other.mQ1Coords;
+    mQ2Coords = other.mQ2Coords;
+    mQ3Coords = other.mQ3Coords;
+
+    /* Initialize coefficient pointers to NULL. */
+    pQ1Coeffs = NULL;
+    pQ2Coeffs = NULL;
+    pQ3Coeffs = NULL;
+
+    /* If other has non-null pointers to coefficients, allocate memory. */
+    if (other.pQ1Coeffs != NULL){
+        pQ1Coeffs = new double [ other.mQ1Coords.size() *
+                                    (other.mMaxOrder+1) ];
+    }
+    if (other.pQ2Coeffs != NULL){
+        pQ2Coeffs = new double [ other.mQ2Coords.size() *
+                                    (other.mMaxOrder+1) ];
+    }
+    if (other.pQ3Coeffs != NULL){
+        pQ3Coeffs = new double [ other.mQ3Coords.size() *
+                                    (other.mMaxOrder+1) ];
+    }
+
+    /* If other has non-null pointers to coefficients, copy them. */
+    if (other.pQ1Coeffs != NULL){
+        std::memcpy( pQ1Coeffs, other.pQ1Coeffs,
+                            other.mQ1Coords.size() *
+                            (other.mMaxOrder+1) *
+                                    sizeof(double) );
+    }
+    if (other.pQ2Coeffs != NULL){
+        std::memcpy( pQ2Coeffs, other.pQ2Coeffs,
+                            other.mQ2Coords.size() *
+                            (other.mMaxOrder+1) *
+                                    sizeof(double) );
+    }
+    if (other.pQ3Coeffs != NULL){
+        std::memcpy( pQ3Coeffs, other.pQ3Coeffs,
+                            other.mQ3Coords.size() *
+                            (other.mMaxOrder+1) *
+                                    sizeof(double) );
+    }
+
+    /* Copy mMaxOrder. */
+    mMaxOrder = other.mMaxOrder;
 }
 
 
 Basic_3D_DiffOp::~Basic_3D_DiffOp ()
 {
+    /* Free memory only if coefficient pointer aren't NULL. */
     if (pQ1Coeffs != NULL) { delete [] pQ1Coeffs; }
     if (pQ2Coeffs != NULL) { delete [] pQ2Coeffs; }
     if (pQ3Coeffs != NULL) { delete [] pQ3Coeffs; }
@@ -81,7 +146,9 @@ Basic_3D_DiffOp::~Basic_3D_DiffOp ()
 
 Basic_3D_DiffOp & Basic_3D_DiffOp::operator= (const Basic_3D_DiffOp & other)
 {
+    /* Avoid self-assignment */
     if (this != &other){
+        /* Copy members. */
         mQ0Point.q1 = other.mQ0Point.q1;
         mQ0Point.q2 = other.mQ0Point.q2;
         mQ0Point.q3 = other.mQ0Point.q3;
@@ -90,30 +157,47 @@ Basic_3D_DiffOp & Basic_3D_DiffOp::operator= (const Basic_3D_DiffOp & other)
         mQ2Coords = other.mQ2Coords;
         mQ3Coords = other.mQ3Coords;
 
+        /* If coefficient pointers aren't NULL, free memory. */
         if (pQ1Coeffs != NULL) { delete [] pQ1Coeffs; }
         if (pQ2Coeffs != NULL) { delete [] pQ2Coeffs; }
         if (pQ3Coeffs != NULL) { delete [] pQ3Coeffs; }
 
-        pQ1Coeffs = new double [ other.mQ1Coords.size() *
-                                    (other.mMaxOrder+1) ];
-        pQ2Coeffs = new double [ other.mQ2Coords.size() *
-                                    (other.mMaxOrder+1) ];
-        pQ3Coeffs = new double [ other.mQ3Coords.size() *
-                                    (other.mMaxOrder+1) ];
+        /* If other has non-null pointers to coefficients, allocate
+         * memory. */
+        if (other.pQ1Coeffs != NULL){
+            pQ1Coeffs = new double [ other.mQ1Coords.size() *
+                                        (other.mMaxOrder+1) ];
+        }
+        if (other.pQ2Coeffs != NULL){
+            pQ2Coeffs = new double [ other.mQ2Coords.size() *
+                                        (other.mMaxOrder+1) ];
+        }
+        if (other.pQ3Coeffs != NULL){
+            pQ3Coeffs = new double [ other.mQ3Coords.size() *
+                                        (other.mMaxOrder+1) ];
+        }
 
-        std::memcpy( pQ1Coeffs, other.pQ1Coeffs,
-                         other.mQ1Coords.size() *
-                            (other.mMaxOrder+1) *
-                                 sizeof(double) );
-        std::memcpy( pQ2Coeffs, other.pQ2Coeffs,
-                         other.mQ2Coords.size() *
-                            (other.mMaxOrder+1) *
-                                 sizeof(double) );
-        std::memcpy( pQ3Coeffs, other.pQ3Coeffs,
-                         other.mQ3Coords.size() *
-                            (other.mMaxOrder+1) *
-                                 sizeof(double) );
+        /* If other has non-null pointers to coefficients, copy them. */
+        if (other.pQ1Coeffs != NULL){
+            std::memcpy( pQ1Coeffs, other.pQ1Coeffs,
+                             other.mQ1Coords.size() *
+                                (other.mMaxOrder+1) *
+                                     sizeof(double) );
+        }
+        if (other.pQ2Coeffs != NULL){
+            std::memcpy( pQ2Coeffs, other.pQ2Coeffs,
+                             other.mQ2Coords.size() *
+                                (other.mMaxOrder+1) *
+                                     sizeof(double) );
+        }
+        if (other.pQ3Coeffs != NULL){
+            std::memcpy( pQ3Coeffs, other.pQ3Coeffs,
+                             other.mQ3Coords.size() *
+                                (other.mMaxOrder+1) *
+                                     sizeof(double) );
+        }
 
+        /* Copy mMaxOrder. */
         mMaxOrder = other.mMaxOrder;
     }
 
@@ -124,6 +208,7 @@ Basic_3D_DiffOp & Basic_3D_DiffOp::operator= (const Basic_3D_DiffOp & other)
 double Basic_3D_DiffOp::fEvalQ1Diff (const unsigned &  order,
                                      const QGrid    & q1Vals)
 {
+    /* If arguments invalid, throw exception. */
     if (order > mMaxOrder){
         throw std::invalid_argument("given order higher than max");
     }
@@ -136,10 +221,12 @@ double Basic_3D_DiffOp::fEvalQ1Diff (const unsigned &  order,
         throw std::invalid_argument("too much values at grid points given");
     }
 
+    /* Extract proper coefficients. */
     double * q1_k_coeffs = FornbergGetCoeffList(pQ1Coeffs,
                                                 mQ1Coords.size(),
                                                 order);
     
+    /* Return partial derivative of given order. */
     return FornbergKDerivEval(q1_k_coeffs,
                               &q1Vals[0], q1Vals.size());
 }
@@ -148,6 +235,7 @@ double Basic_3D_DiffOp::fEvalQ1Diff (const unsigned &  order,
 double Basic_3D_DiffOp::fEvalQ2Diff (const unsigned &  order,
                                      const QGrid    & q2Vals)
 {
+    /* If arguments invalid, throw exception. */
     if (order > mMaxOrder){
         throw std::invalid_argument("given order higher than max");
     }
@@ -160,10 +248,12 @@ double Basic_3D_DiffOp::fEvalQ2Diff (const unsigned &  order,
         throw std::invalid_argument("too much values at grid points given");
     }
 
+    /* Extract proper coefficients. */
     double * q2_k_coeffs = FornbergGetCoeffList(pQ2Coeffs,
                                                 mQ2Coords.size(),
                                                 order);
     
+    /* Return partial derivative of given order. */
     return FornbergKDerivEval(q2_k_coeffs,
                               &q2Vals[0], q2Vals.size());
 }
@@ -172,6 +262,7 @@ double Basic_3D_DiffOp::fEvalQ2Diff (const unsigned &  order,
 double Basic_3D_DiffOp::fEvalQ3Diff (const unsigned &  order,
                                      const QGrid    & q3Vals)
 {
+    /* If arguments invalid, throw exception. */
     if (order > mMaxOrder){
         throw std::invalid_argument("given order higher than max");
     }
@@ -184,10 +275,12 @@ double Basic_3D_DiffOp::fEvalQ3Diff (const unsigned &  order,
         throw std::invalid_argument("too much values at grid points given");
     }
 
+    /* Extract proper coefficients. */
     double * q3_k_coeffs = FornbergGetCoeffList(pQ3Coeffs,
                                                 mQ3Coords.size(),
                                                 order);
     
+    /* Return partial derivative of given order. */
     return FornbergKDerivEval(q3_k_coeffs,
                               &q3Vals[0], q3Vals.size());
 }
--- src/basic_3D_diffop.h
+++ src/basic_3D_diffop.h
@@ -1,7 +1,7 @@
 /*
  * File: basic_3D_diffop.h
  * Author(s): P Kuszaj
- * Last changed: 27.01.2015
+ * Last changed: 28.01.2015
  *
  * A header file providing Basic_3D_DiffOp class. It can be used as
  * a parent class for defining partial differential operators classes
@@ -24,7 +24,8 @@ namespace GridDiff
  *
  * Provides basic framework for calculating numerical partial differential
  * operators of given order at a particular point using arbitrary grid points
- * expressed in any orthonormal parametrization of R^3.
+ * expressed in any orthonormal parametrization of R^3 acting on some scalar
+ * function with known values at grid nodes.
  *
  * Class holds position, at which calculations will be performed (mQ0Point),
  * grid points coordinates along a paricular axis (it is assumed, that grid
@@ -32,7 +33,9 @@ namespace GridDiff
  * coefficients used in discrete dervatives calculation (automatically
  * optimized) and maximal derivative order.
  *
- * Basic_3D_DiffOp class has no public function member for calculating
+ * Basic_3D_DiffOp class has no public function member for evaluating any
+ * differential operator for given point set. User has to implement appropriate
+ * method on his/her own in child class inheriting from this one.
  *
  * Given grid point coordinates can be given in any order, but they have to
  * be unique. mQ0Point coordinates can coincide with those in mQiCoords
@@ -46,20 +49,69 @@ namespace GridDiff
  * a local subgrid and loop over all grid points using the same Basic_3D_DiffOp
  * instance, without redefining mQ0Point and/or mQiCoords and recalculating
  * values stored in pQiCoeffs.
+ *
+ * This class also provides three protected function members fEvalQiDiff
+ * (i=1,2,3) evaluating kth partial derivatives d^k/dqi^k at mQ0Point. User
+ * should use them for defining differential operators in his/her child
+ * classes parented by Basic_3D_DiffOp. For instance: a differential operator:
+ *     Lf(q1,q2,q3) = 3/2*d^2f/dq1^2 + 2*df/dq2 - 1/4*df/dq3
+ * should be implemented as:
+ *     L(q1Vals,q2Vals,q3Vals) = 1.50 * fEvalQ1Diff(2, q1Vals)
+ *                             + 2.00 * fEvalQ2Diff(1, q2Vals)
+ *                             - 0.25 * fEvalQ3Diff(3, q3Vals)
+ * with qiVals being function values for grid points on qi axis.
  */
 class Basic_3D_DiffOp
 {
     private:
+        /* Points coordinates as which operator is evaluated. */
         QPoint     mQ0Point;
+        /* Grid point coordinates at qi axis (i=1,2,3). */
         QGrid      mQ1Coords,
                    mQ2Coords,
                    mQ3Coords;
+        /* Coefficient used in calculating kth partial derivative along qi
+         * axis (i=1,2,3). */
         double   * pQ1Coeffs,
                  * pQ2Coeffs,
                  * pQ3Coeffs;
+        /* Highest derivative order, which class can use. */
         unsigned   mMaxOrder;
 
     protected:
+        /*
+         * fEvalQiDiff() (i=1,2,3)
+         *
+         * Returns kth numerical partial derivative along qi axis using given
+         * function values at grid points. Given order has to be at most equal
+         * to mMaxOrder.
+         *
+         * -----------
+         *  Arguments
+         * -----------
+         * const unsigned & order
+         *     Derivative order. If zero, performs polynomial interpolation.
+         *     Cannot be larger than mMaxOrder.
+         *
+         * const QGrid & qiVals
+         *     Function values at grid points on qi axis. Order has to
+         *     correspond to grid points order. Has to be the same size
+         *     as mQiCoords.
+         *
+         * ---------
+         *  Returns
+         * ---------
+         * Double value equal to numerical partial derivative d^k/dqi^k at
+         * mQ0Point. If order is 0, returns polynomially interpolated function
+         * value at this point.
+         * 
+         * ------------
+         *  Exceptions
+         * ------------
+         * std::invalid_argument if:
+         *     * Given order larger than allowed (order > mMaxOrder)
+         *     * qiVals size not equal to number of grid points along qi axis
+         */
         double fEvalQ1Diff (const unsigned &  order,
                             const QGrid    & q1Vals);
         double fEvalQ2Diff (const unsigned &  order,
@@ -68,17 +120,83 @@ class Basic_3D_DiffOp
                             const QGrid    & q3Vals);
 
     public:
+        /*************
+         * LIFECYCLE *
+         ************/
+
+        /*
+         * Constructor
+         *
+         * -----------
+         *  Arguments
+         * -----------
+         * const QPoint & q0Point
+         *     Point at which evaluations will be performed.
+         *
+         * const QGrid & q1Coords
+         * const QGrid & q2Coords
+         * const QGrid & q3Coords
+         *     Grid point positions along axes q1, q2 and q3. For each axis
+         *     those positions have to be unique, otherwise dire numerical
+         *     errors occur.
+         *
+         * const unsigned & MaxOrder
+         *     Highest derivative used in the differential operator.
+         *
+         * ------------
+         *  Exceptions
+         * ------------
+         * std::invalid_argument if:
+         *     * Any of qiCoords is of size < 2
+         *     * Any of qiCoords is of size < MaxOrder
+         */
         Basic_3D_DiffOp (const QPoint   &  q0Point,
                          const QGrid    & q1Coords,
                          const QGrid    & q2Coords,
                          const QGrid    & q3Coords,
                          const unsigned & MaxOrder);
 
+        /*
+         * Copy constructor
+         *
+         * -----------
+         *  Arguments
+         * -----------
+         * const Basic_3D_DiffOp & other
+         *     Other instance of Basic_3D_DiffOp class.
+         *
+         * ------------
+         *  Exceptions
+         * ------------
+         * None.
+         */
         Basic_3D_DiffOp (const Basic_3D_DiffOp & other);
 
+        /*
+         * Virtual destructor
+         *
+         * ------------
+         *  Exceptions
+         * ------------
+         * None.
+         */
         virtual ~Basic_3D_DiffOp ();
 
 
+        /*************
+         * OPERATORS *
+         ************/
+
+        /*
+         * Copy operator
+         *
+         * Does nothing in case of self-assignment attempt.
+         *
+         * ------------
+         *  Exceptions
+         * ------------
+         * None.
+         */
         Basic_3D_DiffOp & operator= (const Basic_3D_DiffOp & other);
 
 }; /* class Basic_3D_DiffOp */
